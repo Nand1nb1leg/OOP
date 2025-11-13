67f30feb0b86ca5a98fb6af923338202c3419364
Tsedo-hash <tserendorj0229@yahoo.com>
2025-11-13 21:01:56 +0800

    Implement Circle class with perimeter and area methods

--- circle.cpp
+++ circle.cpp
@@ -1,1 +1,15 @@
+class Circle : public TwoDShape {
+public:
+    Circle(float cx, float cy, float r, const char* shapeName) : TwoDShape(cx, cy, r, shapeName) {}
+
+    float getPerimeter() override { return 2 * M_PI * this->side; }
+    float getArea() override { return M_PI * this->side * this->side; }
+
+    void print() override {
+        cout << this->name << " (Circle) details:" << endl
+             << "Center: (" << this->pointA.getX() << ", " << this->pointA.getY() << ")" << endl
+             << "Radius: " << this->side << endl
+             << "Perimeter: " << this->getPerimeter() << ", Area: " << this->getArea() << endl;
+    }
+};
 
